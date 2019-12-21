4521ff6edf51346203665c5a5732fe825dd1a326
George Thomas <georget.87@gmail.com>
2019-12-21 17:29:56 +0530

    Setting up our React app

---
+++ src/config.js
@@ -0,0 +1,16 @@
+export default {
+    s3: {
+      REGION: "YOUR_S3_UPLOADS_BUCKET_REGION",
+      BUCKET: "YOUR_S3_UPLOADS_BUCKET_NAME"
+    },
+    apiGateway: {
+      REGION: "us-east-2",
+      URL: "https://emj1htdrs0.execute-api.us-east-2.amazonaws.com/prod"
+    },
+    cognito: {
+      REGION: "us-east-2",
+      USER_POOL_ID: "us-east-2_te0sPCQIA",
+      APP_CLIENT_ID: "4f07tbp96fn06r9kms61t3tc92",
+      IDENTITY_POOL_ID: "us-east-2:befdd32f-a8ef-4910-85cd-9ea33a6725a5"
+    }
+  };
